58661411efe1577c3f7f11b7d0ecffc95fd31479
unknown <phucchi12112004@gmail.com>
2025-03-07 18:26:43 +0700

    install handlebars express and basic routes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
-const express = require('express')
-import handlebars from './../node_modules/handlebars/lib/handlebars/compiler/parser';
-const morgan = require('morgan')
-import { engine } from 'express-handlebars';
+const path = require('path');
+const { engine } = require('express-handlebars');
+const express = require('express');
+const morgan = require('morgan');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+//http logger 
 app.use(morgan('combined'));
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+//template engine
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/tin-tuc', (req, res) => {
-  res.send('hi World!')
+app.get('/', (req, res) => {
+  res.render('home')
 })
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
